7dcb2238648c04115d0b6630da548221b2981c70
dinesh <dineshsutharxii@gmail.com>
2025-08-06 21:51:15 +0530

    Added POST, PUT and DELETE request methods

--- tests/test_jsonplaceholder_api.py
+++ tests/test_jsonplaceholder_api.py
@@ -1,6 +1,6 @@
 import pytest
 #from electrolux_assignment.utils.api_client import get_posts, get_post_by_id
-from utils.api_client import get_posts, get_post_by_id
+from utils.api_client import get_posts, get_post_by_id, add_new_post, update_post, delete_post
 
 
 def test_get_all_posts(base_url):
@@ -31,3 +31,45 @@ def test_get_post_by_valid_id(base_url, post_id, expected_posts):
 def test_get_post_by_invalid_id(base_url, invalid_id):
     response = get_post_by_id(base_url, invalid_id)
     assert response.status_code == 404 or response.json() == {}
+
+
+@pytest.mark.parametrize('payload', [
+    {
+        "userId": 1,
+        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+    {
+        "userId": 1,
+        "title": "eum et est occaecati",
+        "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    },
+    {
+        "userId": 1,
+        "title": "nesciunt quas odio",
+        "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    }
+])
+def test_add_new_post(base_url, payload):
+    response = add_new_post(base_url, payload)
+    assert response.status_code == 201
+    json_res = response.json()
+    assert json_res['title'] == payload['title']
+    assert json_res['body'] == payload['body']
+
+
+@pytest.mark.parametrize("post_id, payload", [(1, {"id": 1, "title": "Post One", "body": "Body One", "userId": 1}),
+                                              (5, {"id": 5, "title": "Post Five", "body": "Body Five", "userId": 1}),
+                                              (10, {"id": 10, "title": "Post ten", "body": "Body ten", "userId": 1})])
+def test_update_post(base_url, post_id, payload):
+    response = update_post(base_url, payload, post_id)
+    assert response.status_code == 200
+    json_res = response.json()
+    assert json_res['title'] == payload['title']
+    assert json_res['body'] == payload['body']
+
+
+@pytest.mark.parametrize("post_id", [1, 5, 10, 50, 100])
+def test_delete_post(base_url, post_id):
+    response = delete_post(base_url, post_id)
+    assert response.status_code == 200
--- utils/api_client.py
+++ utils/api_client.py
@@ -16,3 +16,30 @@ def get_post_by_id(base_url, post_id):
     response = requests.get(url)
     logging.info(f"Response: {response.status_code}")
     return response
+
+
+def add_new_post(base_url, payload):
+    url = f"{base_url}/posts"
+    logging.info(f'Post {url}')
+    response = requests.post(url=url, json=payload)
+    logging.info(f"Response: {response.status_code}")
+    logging.info(f"Response: {response.json()}")
+    return response
+
+
+def update_post(base_url, payload, id):
+    url = f"{base_url}/posts/{id}"
+    logging.info(f'Put {url}')
+    response = requests.put(url=url, json=payload)
+    logging.info(f"Response: {response.status_code}")
+    logging.info(f"Response: {response.json()}")
+    return response
+
+
+def delete_post(base_url, id):
+    url = f"{base_url}/posts/{id}"
+    logging.info(f'Delete {url}')
+    response = requests.put(url=url)
+    logging.info(f"Response: {response.status_code}")
+    logging.info(f"Response: {response.json()}")
+    return response
